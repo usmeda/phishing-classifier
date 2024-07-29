90b81824e98b99fe795a0302bff4a3c18b3a2ded
Ujwal Shreenag Meda <usmeda@gmail.com>
2024-07-29 15:32:04 +0530

    Visualization and initial setup

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # phishing-classifier
 This is a machine learning project 1
+
+'''
+pip install -e . to run the setup.py
+
+'''
--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src'
-    version='0.0.1'
-    author='UjwalMeda'
-    author_email='usmeda@gmail.com'
+    name='src',
+    version='0.0.1',
+    author='UjwalMeda',
+    author_email='usmeda@gmail.com',
     packages=find_packages(),
     install_requires=[],
 )
